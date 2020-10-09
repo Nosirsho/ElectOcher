0fdae97164920d83206e9fc9dca3ee6b7dc6ecb3
Kholmamadov N <Pamirikanec-m17@mail.ru>
2020-10-09 15:24:42 +0400

    add peport for datae

--- Controllers/OtchetController.cs
+++ Controllers/OtchetController.cs
@@ -10,16 +10,23 @@ namespace ElectrOcher.Controllers
 {
     public class OtchetController : Controller
     {
-        // GET: Otchet
         List<Talon> tl = TalonServ.talonList;
 
 
-
+        [HttpGet]
         public  ViewResult Index()
         {
             return View(tl);
         }
 
+        [HttpPost]
+        public ViewResult Index(DateTime sDate, DateTime eDate)
+        {
+
+            var filteredData = tl.Where(t => t.AcceptTime >= sDate && t.AcceptTime <= eDate);
+            return View(filteredData);
+            //return View(tl.Where(a => a.TalonTime >= sDate && a.TalonTime <= eDate));
+        }
 
     }
 }
